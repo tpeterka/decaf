86cf5f8a76facdd9f674fb727321652a826523ab
Tom Peterka <tpeterka@mcs.anl.gov>
2015-03-25 19:23:58 -0500

    working on examples/lammps-workflow/python/driver.py

--- examples/lammps-workflow/python/driver.py
+++ examples/lammps-workflow/python/driver.py
@@ -1,27 +1,35 @@
+import networkx as nx
 
 # --- set your options here ---
 
 # path to .so module
-path = '/Users/tpeterka/software/decaf/install/examples/lammps-workflow/python/libpy_lammps.so'
+path = '/Users/tpeterka/software/decaf/install/examples/lammps-workflow/python/libpy_lammps_workflow.so'
 
-# communicator sizes and time steps
-class DecafSizes:
-    prod_size    = 4      # size of producer communicator
-    dflow_size   = 2      # size of dataflow communicator
-    con_size     = 2      # size of consumer communicator
-    prod_start   = 0      # starting rank of producer communicator in the world
-    dflow_start  = 4      # starting rank of dataflow communicator in the world
-    con_start    = 6      # starting rank of consumer communicator in the world
-    con_nsteps   = 0      # number of consumer (decaf) time steps
+# define workflow graph
+w = nx.DiGraph()
+w.add_nodes_from(["l", "p1", "p2", "p3"])
+w.add_edges_from([("l", "p1"), ("l", "p2"), ("p2", "p3")])
+w.nodes()
+w.degree()
 
-# total number of time steps; this is the user's variable, not part of decaf
-prod_nsteps  = 0
+# # communicator sizes and time steps
+# class DecafSizes:
+#     prod_size    = 4      # size of producer communicator
+#     dflow_size   = 2      # size of dataflow communicator
+#     con_size     = 2      # size of consumer communicator
+#     prod_start   = 0      # starting rank of producer communicator in the world
+#     dflow_start  = 4      # starting rank of dataflow communicator in the world
+#     con_start    = 6      # starting rank of consumer communicator in the world
+#     con_nsteps   = 1      # number of consumer (decaf) time steps
 
-# lammps input file
-infile = "/Users/tpeterka/software/decaf/examples/lammps-workflow/in.melt"
+# # total number of time steps; this is the user's variable, not part of decaf
+# prod_nsteps  = 1
 
-# --- do not edit below this point --
+# # lammps input file
+# infile = "/Users/tpeterka/software/decaf/examples/lammps-workflow/in.melt"
 
-import imp
-driver = imp.load_dynamic('driver', path)
-driver.pyrun(DecafSizes, prod_nsteps, infile)
+# # --- do not edit below this point --
+
+# import imp
+# driver = imp.load_dynamic('driver', path)
+# driver.pyrun(DecafSizes, prod_nsteps, infile)
