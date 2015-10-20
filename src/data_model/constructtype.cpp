7b7e0c1a2f95b923735f20389ca006b99e3fafad
Matthieu Dreher <mdreher@anl.gov>
2015-10-20 16:30:24 -0500

    Fix compilation and debug test2count

--- src/data_model/constructtype.cpp
+++ src/data_model/constructtype.cpp
@@ -1039,7 +1039,7 @@ ConstructData::updateMetaData()
         nbFields_++;
     }
 
-    //std::cout<<"Number of items after insertion : "<<nbItems_<<std::endl;
+    std::cout<<"Number of items after insertion : "<<nbItems_<<std::endl;
 
     return true;
 }
