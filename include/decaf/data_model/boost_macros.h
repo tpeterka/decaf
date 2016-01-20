ac0be7833a7c79cfc7a02af50ae3f03727a53a71
Tom Peterka <tpeterka@mcs.anl.gov>
2016-01-20 12:49:40 -0600

    moved boost macros into separate .h file (forgot to git add the file in previous commit)

---
+++ include/decaf/data_model/boost_macros.h
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------------
+//
+// boost macros for all serializable objects
+// user applications need to include this file
+//
+// Matthieu Dreher
+// Argonne National Laboratory
+// 9700 S. Cass Ave.
+// Argonne, IL 60439
+// mdreher@anl.gov
+//
+//--------------------------------------------------------------------------
+
+#include <boost/serialization/export.hpp>
+
+BOOST_CLASS_EXPORT_GUID(decaf::BaseConstructData,"BaseConstructData")
+
+BOOST_CLASS_EXPORT_GUID(decaf::Block<1>,"decaf::Block<1>")
+BOOST_CLASS_EXPORT_GUID(decaf::Block<2>,"decaf::Block<2>")
+BOOST_CLASS_EXPORT_GUID(decaf::Block<3>,"decaf::Block<3>")
+BOOST_CLASS_EXPORT_GUID(decaf::Block<4>,"decaf::Block<4>")
+BOOST_CLASS_EXPORT_GUID(decaf::Block<5>,"decaf::Block<5>")
