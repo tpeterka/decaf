bed07e2f7e3c7a6b483149f1f2c8c7e527b895bf
Tom Peterka <tpeterka@mcs.anl.gov>
2016-05-26 15:42:37 -0500

    minor style changes

--- include/decaf/data_model/arrayfield.hpp
+++ include/decaf/data_model/arrayfield.hpp
@@ -9,85 +9,100 @@
 namespace decaf {
 
 
-template <typename T>
-class ArrayField : public BaseField {
+    template <typename T>
+    class ArrayField : public BaseField {
 
-public:
-    ArrayField(std::shared_ptr<BaseConstructData> ptr)
-    {
-        ptr_ = std::dynamic_pointer_cast<ArrayConstructData<T> >(ptr);
-        if(!ptr_){
-            std::cerr<<"ERROR : Unable to cast pointer to ArrayConstructData<T> when using a ArrayField."<<std::endl;
-            ptr_.reset();
-        }
-    }
+    public:
+        ArrayField(std::shared_ptr<BaseConstructData> ptr)
+            {
+                ptr_ = std::dynamic_pointer_cast<ArrayConstructData<T> >(ptr);
+                if(!ptr_)
+                {
+                    std::cerr<<"ERROR : Unable to cast pointer to ArrayConstructData<T> when using a ArrayField."<<std::endl;
+                    ptr_.reset();
+                }
+            }
 
-   ArrayField(mapConstruct map = mapConstruct())
-   {
-       ptr_ = std::make_shared<ArrayConstructData<T> >(map);
-   }
+        ArrayField(mapConstruct map = mapConstruct())
+            {
+                ptr_ = std::make_shared<ArrayConstructData<T> >(map);
+            }
 
-   ArrayField(T* array, int size, int element_per_items, bool owner = false, mapConstruct map = mapConstruct())
-   {
-       ptr_ = std::make_shared<ArrayConstructData<T> >(array, size, element_per_items, owner, map);
-   }
+        ArrayField(T* array,
+                   int size,
+                   int element_per_items,
+                   bool owner = false,
+                   mapConstruct map = mapConstruct())
+            {
+                ptr_ = std::make_shared<ArrayConstructData<T> >
+                    (array, size, element_per_items, owner, map);
+            }
 
-   ArrayField(T* array, int size, int element_per_items, int capacity, bool owner = false, mapConstruct map = mapConstruct())
-   {
-       ptr_ = std::make_shared<ArrayConstructData<T> >( array, size, element_per_items, capacity, owner, map);
-   }
+        ArrayField(T* array,
+                   int size,
+                   int element_per_items,
+                   int capacity,
+                   bool owner = false,
+                   mapConstruct map = mapConstruct())
+            {
+                ptr_ = std::make_shared<ArrayConstructData<T> >
+                    ( array, size, element_per_items, capacity, owner, map);
+            }
 
-   ArrayField(std::vector<std::pair<T*, unsigned int> > segments, int element_per_items, mapConstruct map = mapConstruct())
-   {
-       ptr_ = std::make_shared<ArrayConstructData<T> >(segments, element_per_items, map);
-   }
+        ArrayField(std::vector<std::pair<T*, unsigned int> > segments,
+                   int element_per_items,
+                   mapConstruct map = mapConstruct())
+            {
+                ptr_ = std::make_shared<ArrayConstructData<T> >(segments, element_per_items, map);
+            }
 
-   virtual ~ArrayField(){}
+        virtual ~ArrayField(){}
 
-   virtual BaseConstructData* operator -> () const
-   {
-       return ptr_.get();
-   }
+        virtual BaseConstructData* operator -> () const
+            {
+                return ptr_.get();
+            }
 
-   virtual std::shared_ptr<BaseConstructData> getBasePtr()
-   {
-       return ptr_;
-   }
+        virtual std::shared_ptr<BaseConstructData> getBasePtr()
+            {
+                return ptr_;
+            }
 
-   std::shared_ptr<ArrayConstructData<T> > getPtr()
-   {
-       return ptr_;
-   }
+        std::shared_ptr<ArrayConstructData<T> > getPtr()
+            {
+                return ptr_;
+            }
 
-   bool empty()
-   {
-       return ptr_.use_count() == 0;
-   }
+        bool empty()
+            {
+                return ptr_.use_count() == 0;
+            }
 
-   T* getArray()
-   {
-       return ptr_->getArray();
-   }
+        T* getArray()
+            {
+                return ptr_->getArray();
+            }
 
-   virtual int getNbItems()
-   {
-       return ptr_->getNbItems();
-   }
+        virtual int getNbItems()
+            {
+                return ptr_->getNbItems();
+            }
 
-   virtual operator bool() const {
-         return ptr_ ? true : false;
-       }
+        virtual operator bool() const
+            {
+                return ptr_ ? true : false;
+            }
 
 
 
-private:
-    std::shared_ptr<ArrayConstructData<T> > ptr_;
-};
+    private:
+        std::shared_ptr<ArrayConstructData<T> > ptr_;
+    };
 
-typedef ArrayField<int> ArrayFieldi;
-typedef ArrayField<unsigned int> ArrayFieldu;
-typedef ArrayField<float> ArrayFieldf;
-typedef ArrayField<double> ArrayFieldd;
+    typedef ArrayField<int> ArrayFieldi;
+    typedef ArrayField<unsigned int> ArrayFieldu;
+    typedef ArrayField<float> ArrayFieldf;
+    typedef ArrayField<double> ArrayFieldd;
 
 } // namespace
 #endif
