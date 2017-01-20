2c6da077dfdeb443627cecbb6cbc3dfd0b3bb277
Matthieu Dreher <mdreher@anl.gov>
2017-01-20 16:00:59 -0600

    Add a new example to test a buffer mechanism.

---
+++ examples/buffer/prod.cpp
@@ -0,0 +1,77 @@
+//---------------------------------------------------------------------------
+//
+// 2-node producer-consumer coupling example with buffering mechanism
+//
+// prod (1 procs) - con (1 procs)
+//
+// entire workflow takes 3 procs (1 dataflow procs between prod and con)
+//
+// Matthieu Dreher
+// Argonne National Laboratory
+// 9700 S. Cass Ave.
+// Argonne, IL 60439
+// mdreher@anl.gov
+//
+//--------------------------------------------------------------------------
+
+#include <decaf/decaf.hpp>
+#include <decaf/data_model/pconstructtype.h>
+#include <decaf/data_model/simplefield.hpp>
+#include <decaf/data_model/boost_macros.h>
+
+#include <assert.h>
+#include <math.h>
+#include <mpi.h>
+#include <map>
+#include <cstdlib>
+
+using namespace decaf;
+using namespace std;
+
+// producer
+void prod(Decaf* decaf)
+{
+    // produce data for some number of timesteps
+    for (int timestep = 0; timestep < 2; timestep++)
+    {
+        fprintf(stderr, "producer timestep %d\n", timestep);
+
+        // the data in this example is just the timestep; add it to a container
+        //shared_ptr<SimpleConstructData<int> > data =
+        //    make_shared<SimpleConstructData<int> >(timestep);
+        SimpleFieldi data(timestep);
+        pConstructData container;
+        container->appendData("var", data,
+                              DECAF_NOFLAG, DECAF_PRIVATE,
+                              DECAF_SPLIT_KEEP_VALUE, DECAF_MERGE_ADD_VALUE);
+
+        // send the data on all outbound dataflows
+        // in this example there is only one outbound dataflow, but in general there could be more
+        decaf->put(container);
+    }
+
+    // terminate the task (mandatory) by sending a quit message to the rest of the workflow
+    fprintf(stderr, "producer terminating\n");
+    decaf->terminate();
+}
+
+int main(int argc,
+         char** argv)
+{
+    Workflow workflow;
+    Workflow::make_wflow_from_json(workflow, "buffer.json");
+
+    MPI_Init(NULL, NULL);
+
+    // create decaf
+    Decaf* decaf = new Decaf(MPI_COMM_WORLD, workflow);
+
+    // run decaf
+    prod(decaf);
+
+    // cleanup
+    delete decaf;
+    MPI_Finalize();
+
+    return 0;
+}
